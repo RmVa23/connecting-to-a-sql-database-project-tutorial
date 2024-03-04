a08353cd290951d37fc2a2d8da352fe4cc468ca8
RmVa23 <148461819+RmVa23@users.noreply.github.com>
2024-03-04 16:14:58 +0000

    intento conexion DB render

--- src/app.py
+++ src/app.py
@@ -7,8 +7,20 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # 1) Connect to the database here using the SQLAlchemy's create_engine function
+connection_string = "postgres://bambi_user:VNw81hK03f5A2pvNlyexCWdffjrb6vPj@dpg-cnitbq8l5elc73feo090-a.frankfurt-postgres.render.com/bambi"
+engine = create_engine(connection_string)
+
+            # Intentar establecer la conexión a la base de datos
+try:
+    with engine.connect() as connection:
+        # Realizar operaciones en la base de datos aquí
+        print("Conexión exitosa a la base de datos")
+except Exception as e:
+    print(f"Error al conectar a la base de datos: {e}")
+
 
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+
 
 # 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
 
